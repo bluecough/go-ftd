dd863d3a34a184349f47ed766b8455b00a23a16f
bluecough <9311254+bluecough@users.noreply.github.com>
2021-06-04 19:07:10 -0400

    removed unsed

--- fdmDeploy.go
+++ fdmDeploy.go
@@ -1,14 +1,34 @@
 package goftd
 import (
-	"encoding/json"
-	"fmt"
 	"strconv"
 
-	"github.com/golang/glog"
 )
 
-func (f *FTD) postDeploy(limit int) error {
+type DeployObject struct {
+	ReferenceObject
+	Description     string `json:"description,omitempty"`
+	SubType         string `json:"subType"`
+	Value           string `json:"value"`
+	IsSystemDefined bool   `json:"isSystemDefined,omitempty"`
+	Links           *Links `json:"links,omitempty"`
+}
+
+// Reference Returns a reference object
+func (n *DeployObject) Reference() *ReferenceObject {
+	r := ReferenceObject{
+		ID:      n.ID,
+		Name:    n.Name,
+		Version: n.Version,
+		Type:    n.Type,
+	}
+
+	return &r
+}
+
+
+func (f *FTD) postDeploy(n *DeployObject, limit int) error {
 	var err error
+	n.Type = "deployobject"
 	endpoint := apiDeploy
 
 	filter := make(map[string]string)
